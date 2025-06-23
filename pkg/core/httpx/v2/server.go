48341d768750ecfc4db98116b7b58b1d0d701637
kyerand <iduchungho@gmail.com>
2025-06-23 22:51:43 +0700

    fix: not blocking when request match rule

--- pkg/core/httpx/v2/server.go
+++ pkg/core/httpx/v2/server.go
@@ -59,7 +59,7 @@ func (s *server) Handle(fn func(ctx *Context) error) {
 		handler = s.mdw[i](handler)
 	}
 
-	s.core.Handler = fasthttp.CompressHandler(handler)
+	s.core.Handler = handler
 }
 
 // Shutdown implements platform.Server.
