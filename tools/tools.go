642edc215a1d695b87a40e5d5b9ba37c63568ee2
kyerand <iduchungho@gmail.com>
2025-08-25 10:13:09 +0700

    chore: update sentinez dev tools

--- tools/tools.go
+++ tools/tools.go
@@ -10,9 +10,11 @@ package tools
 import (
 	_ "github.com/bufbuild/buf/cmd/buf"
 	_ "github.com/codesenberg/bombardier"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	_ "github.com/sentinez/sentinez/tools/cmd/protoc-gen-go-sntzmodels"
+	_ "github.com/sentinez/sentinez/tools/cmd/protoc-gen-go-sntzopts"
+	_ "github.com/sentinez/sentinez/tools/cmd/ruleparser-sentinez"
 	_ "github.com/vektra/mockery/v2"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
