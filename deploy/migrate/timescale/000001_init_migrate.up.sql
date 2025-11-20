4c511afff2daa2aa70e1ec05bf6bb955aee999ef
Duc-Hung Ho <iduchungho@gmail.com>
2025-11-20 21:35:27 +0700

    chore: add README and remove unnessesary file

--- deploy/migrate/timescale/000001_init_migrate.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE
-    IF NOT EXISTS sensor_data (
-        time TIMESTAMPTZ NOT NULL,
-        device_id TEXT NOT NULL,
-        temperature DOUBLE PRECISION NULL,
-        humidity DOUBLE PRECISION NULL
-    );
