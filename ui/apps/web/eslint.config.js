f1114b401bf623f00a90367c30cab0c38e98ba94
Duc-Hung Ho <iduchungho@gmail.com>
2026-04-03 16:31:06 +0700

    chore: fix pnpm run lint

--- ui/apps/web/eslint.config.js
+++ ui/apps/web/eslint.config.js
@@ -1,4 +1,4 @@
-import { nextJsConfig } from "@sentinez/eslint-config/next-js"
+import { nextJsConfig } from "@sentinez/eslint-config/next"
 
 /** @type {import("eslint").Linter.Config} */
 export default nextJsConfig
